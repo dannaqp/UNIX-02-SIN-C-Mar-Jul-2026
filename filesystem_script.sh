320d3a09435eb8f98a48523c041e242f47b5173a
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-03-27 14:39:19 +0000

    wip: devices as files

---
+++ filesystem_script.sh
@@ -0,0 +1,19 @@
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (filesystem) $ cd /dev # With this command I did a change directory to dev directory
+@dannaqp ➜ /dev $ ls # List of files in /dev
+autofs           kvm           nvram   sdc       tty12  tty28  tty43  tty59   ttyS16  ttyS31       vcs3   vcsu6
+bsg              loop-control  port    sdc1      tty13  tty29  tty44  tty6    ttyS17  ttyS4        vcs4   vfio
+btrfs-control    loop0         ppp     sg0       tty14  tty3   tty45  tty60   ttyS18  ttyS5        vcs5   vga_arbiter
+core             loop1         psaux   sg1       tty15  tty30  tty46  tty61   ttyS19  ttyS6        vcs6   vhost-net
+cpu_dma_latency  loop2         ptmx    sg2       tty16  tty31  tty47  tty62   ttyS2   ttyS7        vcsa   vhost-vsock
+cuse             loop3         ptp0    sg3       tty17  tty32  tty48  tty63   ttyS20  ttyS8        vcsa1  vmbus
+dma_heap         loop4         pts     shm       tty18  tty33  tty49  tty7    ttyS21  ttyS9        vcsa2  zero
+dri              loop5         random  snapshot  tty19  tty34  tty5   tty8    ttyS22  ttyprintk    vcsa3  zfs
+ecryptfs         loop6         rfkill  sr0       tty2   tty35  tty50  tty9    ttyS23  udmabuf      vcsa4
+fb0              loop7         root    stderr    tty20  tty36  tty51  ttyS0   ttyS24  uhid         vcsa5
+fd               mapper        rtc0    stdin     tty21  tty37  tty52  ttyS1   ttyS25  uinput       vcsa6
+full             mcelog        sda     stdout    tty22  tty38  tty53  ttyS10  ttyS26  urandom      vcsu
+fuse             mem           sda1    tty       tty23  tty39  tty54  ttyS11  ttyS27  userfaultfd  vcsu1
+hpet             mqueue        sda14   tty0      tty24  tty4   tty55  ttyS12  ttyS28  userio       vcsu2
+hwrng            net           sda15   tty1      tty25  tty40  tty56  ttyS13  ttyS29  vcs          vcsu3
+input            null          sdb     tty10     tty26  tty41  tty57  ttyS14  ttyS3   vcs1         vcsu4
+kmsg             nvme-fabrics  sdb1    tty11     tty27  tty42  tty58  ttyS15  ttyS30  vcs2         vcsu5
