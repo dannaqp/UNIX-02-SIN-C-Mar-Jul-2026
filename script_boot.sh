d6ae13cb82aae387b53c6063f97d9fce19959642
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-04-10 13:28:20 +0000

    wip:route types

---
+++ script_boot.sh
@@ -0,0 +1,4 @@
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot) $ cd calculadora/ # With this command we change directory to calculadora/
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora (boot) $ pwd # With this command shows our directory route
+/workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora (boot) $ cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora # With this command we change directory to /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora
