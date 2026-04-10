6ba1c48c616be6af8c2b980d0ea1382f47d1009e
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-04-10 13:43:01 +0000

    Create and view of a new directory

--- script_boot.sh
+++ script_boot.sh
@@ -1,4 +1,15 @@
-@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot) $ cd calculadora/ # With this command we change directory to calculadora/
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot) $ cd calculadora/ # With this command we change directory to calculadora/ *relative route*
 @dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora (boot) $ pwd # With this command shows our directory route
 /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora
-@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora (boot) $ cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora # With this command we change directory to /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora (boot) $ cd /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora # With this command we change directory to /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora *absolute route*
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/calculadora (boot) $ cd .. # For change to our principal route
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot) $ mkdir proyecto # To create a new directory
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (boot) $ cd proyecto # Change directory to proyecto
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/proyecto (boot) $ ls -lai # Shows visible files
+total 8
+1572988 drwxrwxrwx+ 2 codespace codespace 4096 Apr 10 13:32 .
+1572874 drwxrwxrwx+ 5 codespace root      4096 Apr 10 13:32 ..
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026/proyecto (boot) $ ls -Flai # Shows a list of our current file
+total 8
+1572988 drwxrwxrwx+ 2 codespace codespace 4096 Apr 10 13:32 ./
+1572874 drwxrwxrwx+ 5 codespace root      4096 Apr 10 13:32 ../
