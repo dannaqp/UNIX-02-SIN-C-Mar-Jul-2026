c1d8f9f76fffe052509c13cd6144e902af1b8401
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-05-03 09:21:26 +0000

    2.1 Arguments

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -5,3 +5,11 @@ calculadora                doc_no_cifrado_firmado.txt                       mari
 doc_cifrado.txt            doc_no_cifrado_firmado_binario.txt               marida_doc_no_cifrado.txt          mi_llave_publica.asc
 doc_cifrado_y_firmado.txt  firma_separada_doc_no_cifrado.sig                marida_doc_no_cifrado_firmado.txt  script_linux_cisco_1.sh
 
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (linux_cisco_1) $ ls calculadora # This command shows the files within the calculadora directory, here we use calculadora as argument
+app.py  suma.py
+
+sysadmin@localhost:~$ aptitude moo # This command is a function to manage packages, Only works in some versions of Linux                                              
+There are no Easter Eggs in this program. 
+
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (linux_cisco_1) $ aptitude moo # This command didn't work in codespaces
+bash: aptitude: command not found
