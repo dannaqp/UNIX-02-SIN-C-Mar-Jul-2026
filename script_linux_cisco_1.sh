94084190e66cb6220e0e6ca1b220b7929ead7720
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-05-03 22:50:00 +0000

    7. Permissions

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -175,4 +175,18 @@ root@localhost:~# sl # Execute the entertainment command
 root@localhost:~# exit # To logout from the root user                                         
 logout                                                                                                                                           
 sysadmin@localhost:~$ sudo sl # Execute sl with super user permissions                                     
-[sudo] password for sysadmin:                                                   
+[sudo] password for sysadmin:     
+
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (linux_cisco_1) $ ls # List of files
+LICENSE                                          gnupg_script.sh
+README.md                                        marida_doc_cifrado.txt
+calculadora                                      marida_doc_cifrado_y_firmado.txt
+doc_cifrado.txt                                  marida_doc_no_cifrado.txt
+doc_cifrado_y_firmado.txt                        marida_doc_no_cifrado_firmado.txt
+doc_cifrado_y_firmado_descifrado_y_validado.txt  marida_doc_no_cifrado_firmado_binario.txt
+doc_no_cifrado.txt                               marida_firma_separada_doc_no_cifrado.sig
+doc_no_cifrado_firmado.txt                       marida_llave_publica.asc
+doc_no_cifrado_firmado_binario.txt               mi_llave_publica.asc
+firma_separada_doc_no_cifrado.sig                script_linux_cisco_1.sh
+@dannaqp ➜ /workspaces/UNIX-02-SIN-C-Mar-Jul-2026 (linux_cisco_1) $ ls -l doc_no_cifrado.txt # List only doc_no_cifrado.txt in a long format
+-rw-rw-rw- 1 codespace root 16 May  3 07:06 doc_no_cifrado.txt 
