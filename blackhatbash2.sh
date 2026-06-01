672db6e46533e1f99bbde2d915d3b6f162d97f75
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-06-01 15:25:39 +0000

    Execute the second script

---
+++ blackhatbash2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# All this script does is create a directory, create a file
+
+# within the directory, and then list the contents of the directory.
+
+mkdir mydirectory
+touch mydirectory/myfile
+ls -l mydirectory
