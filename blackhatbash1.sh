d79b182c03c39ce7fa5ed2ed79f6ed46728a982f
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-06-01 14:45:31 +0000

    Environmental variables

---
+++ blackhatbash1.sh
@@ -0,0 +1,11 @@
+[Danna-Simaluisa ✦ ݁˖] ✓ $ bash --version
+
+[Danna-Simaluisa ✦ ݁˖] ✓ $ env
+
+[Danna-Simaluisa ✦ ݁˖] ✓ $ echo ${SHELL}
+
+[Danna-Simaluisa ✦ ݁˖] ✓ $ echo ${RANDOM}
+
+[Danna-Simaluisa ✦ ݁˖] ✓ $ echo ${UID}
+
+[Danna-Simaluisa ✦ ݁˖] ✓ $ echo ${OSTYPE}
