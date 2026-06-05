5c596cc24709e459fcaed1a9ab8a29c7ba11aaae
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-06-05 15:38:33 +0000

    More Control Operators

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -4,3 +4,16 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "El comando lzl falló"
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Bye World!" > output.txt
+cat output.txt
+echo "Goodbye!" >> output.txt
+cat output.txt
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt
