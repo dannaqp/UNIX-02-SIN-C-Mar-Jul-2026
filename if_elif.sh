9fe978948b20f75fd95e69a7bb9b880a157b5ecf
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-06-12 14:31:07 +0000

    Comments on if_elif.sh

--- if_elif.sh
+++ if_elif.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 USER_INPUT="${1}"
+# 1. We begin with an if statement that checks whether the variable
+# USER_INPUT is null.
 if [[ -z "${USER_INPUT}" ]]; then
  echo "You must provide an argument!"
  exit 1
 fi
+# 2. This allows us to exit the script early by using exit
+# 1 if we receive no command line arguments from the user. We then begin a
+# second if condition that uses the file test operator to check whether the
+# input is a file.
 if [[ -f "${USER_INPUT}" ]]; then
  echo "${USER_INPUT} is a file."
+# 3. Below this condition, we use elif to test whether the
+# argument is a directory.
 elif [[ -d "${USER_INPUT}" ]]; then
  echo "${USER_INPUT} is a directory."
+# 4. This condition won’t be tested unless the file
+# test fails. If neither of these conditions is true, the script responds that the
+# argument is neither a file nor a directory
 else
 echo "${USER_INPUT} is not a file or a directory."
 fi
