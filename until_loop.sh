99552c25eb0ba503d995fa82fc3bc5ab64721656
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-06-12 15:10:08 +0000

    Checking a file’s size

---
+++ until_loop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FILE="output.txt" # We set the global variable FILE to contain output.txt
+touch "${FILE}" # The output.txt file is created using touch and the FILE variable.
+until [[ -s "${FILE}" ]]; do # until FILE has more than 0 bits it executes
+ echo "${FILE} is empty..." # Print using the global variable file
+ echo "Checking again in 2 seconds..." # Print to check in 2 seconds
+ sleep 2 # Count 2 seconds to check the until again
+done # When until is already fulfilled
+echo "${FILE} appears to have some content in it!" # When until is already fulfilled 
