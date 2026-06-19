1b7c0a24d12b88b17b2a56cccb2a6eb75dba2f1f
DANNA ALEJANDRA SIMALUISA QUIZHPI <dasimaluisaqu@uide.edu.ec>
2026-06-19 15:50:46 +0000

    delete, print and insert with sed

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -24,3 +24,9 @@ awk '{print $7}' log.txt # Print column 7 of the log.txt file
 grep "42.236.10.117" log.txt | awk '{print $7}' # Filter those that contain that IP ("42.236.10.117") and also print only column 7 of these lines
 sed 's/Mozilla/Godzilla/g' log.txt
 sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+sed 's/ //g' log.txt
+sed '1d' log.txt
+sed '$d' log.txt
+sed '5,7d' log.txt
+sed -n '2,15 p' log.txt 
+sed -i '1d' log.txt
